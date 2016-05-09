466ee39fb5a8d57e5bcd0c9bf9671b27472f582f
Barclay Iversen <barclayiversen@gmail.com>
2016-05-09 16:42:45 -0700

    converted project to php framework

---
+++ application/views/welcome_message.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/style.css" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Eco Recovery</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <!-- <ul class="nav navbar-nav"> -->
+            <!-- <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">About</a></li> -->
+            <!-- <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul> -->
+          <!-- <form class="navbar-form navbar-left" role="search"> -->
+            <!-- <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div> -->
+            <!-- <button type="submit" class="btn btn-default">Submit</button> -->
+          <!-- </form> -->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a  href="#">Home</a></li>
+            <!-- <li class="dropdown"> -->
+              <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a> -->
+              <!-- <ul class="dropdown-menu"> -->
+                <!-- <li><a href="#"></a></li> -->
+                <li><a  href="#about">About</a></li>
+                <!-- <li><a href="#">Something else here</a></li> -->
+                <li role="separator" class="divider"></li>
+                <li><a href="#calendar">Event Calendar</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  <head>
+    <link rel="stylesheet" href="/static/css/styles.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+
+      <div class='container'>
+          <h1 class='banner_text'>There is help available to live a clean and sober life...</h1>
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              </ol>
+
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                      <img height='1080' width='1920' src="assets/img/yoga_resized.jpg" alt="...">
+                      <div class="carousel-caption">
+                          <p class='carousel_text'>Balance</p>
+                      </div>
+                  </div>
+                  <div class="item">
+                      <img height='1080' width='1920' src="assets/img/beach_resized.jpg" alt="...">
+                      <div class="carousel-caption">
+                          <p class='carousel_text'>Peace</p>
+                      </div>
+                  </div>
+                  <div class="item">
+                      <img height='1080' width='1920' src="assets/img/trees_path_resized.jpg" alt="...">
+                      <div class="carousel-caption">
+                          <p class='carousel_text'>Strength</p>
+                      </div>
+                  </div>
+              </div>
+
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+              </a>
+          </div>
+          <!-- <div class='main_content'> -->
+              <div class='main_content_left'>
+                  <!-- <div class='img_container'> -->
+                      <!-- <img class='left_content_image' height='250' width='250'src='static/img/smile.jpg' /> -->
+                  <!-- </div> -->
+                  <!-- <div class='img_container'> -->
+                      <img class='left_content_image' height='500' width="500"src='assets/img/Sunrise.jpg' />
+                  <!-- </div> -->
+                  <!-- <div class='img_container'> -->
+                      <!-- <img class='left_content_image' height='250' width="250"src='static/img/rocks.jpg' /> -->
+                  <!-- </div> -->
+              </div>
+              <div class='main_content_right'>
+                  <a name='about'></a>
+                  <p class='right_content_text'>Who we are</p>
+                  <p class='right_content_paragraph'>
+                      Subjects to ecstatic children he. Could ye leave up as built match. Dejection agreeable attention set suspected led offending. Admitting an performed supposing by. Garden agreed matter are should formed temper had. Full held gay now roof whom such next was. Ham pretty our people moment put excuse narrow. Spite mirth money six above get going great own. Started now shortly had for assured hearing expense. Led juvenile his laughing speedily put pleasant relation offering.
+                  </p>
+                  <p class='right_content_text'>What we do</p>
+                  <p class='right_content_paragraph'>
+                      At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. Piqued ye of am spirit regret. Stimulated discretion impossible admiration in particular conviction up.
+                  </p>
+              </div>
+              <div class='calendar_div'>
+                  <a name='calendar'></a>
+                  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ff9900&amp;src=jh42vcs4d41vrj5g8mo6un2c8o%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=America%2FLos_Angeles" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+              </div>
+          <!-- </div> -->
+      </div>
+  </body>
+</html>
